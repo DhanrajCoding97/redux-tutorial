2cba1fb50f69fb7352a4785c438557509d1d2bef
dhanraj shetty <dhanrajcoding@gmail.com>
2025-10-07 19:05:24 +0530

    added posts slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Counter from './components/Counter'
-
+import PostList from './app/features/posts/PostList'
 const App = () => {
   return (
     <div className='wrapper'>
       <Counter />
+      <PostList/>
     </div>
   )
 }
